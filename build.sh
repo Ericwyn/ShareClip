48d645a4f8994c86ec5103dc13c088dbacf3eed6
Ericwyn <chenchongwei1@xiaomi.com>
2020-02-28 11:43:02 +0800

    fix: fix error in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-echo 'build windows application'
-
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ShareClipClient src/main/ShareClipClient.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ShareClipServer src/main/ShareClipServer.go
-
 echo 'build linux application'
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ShareClipClient.exe src/main/ShareClipClient.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ShareClipServer.exe src/main/ShareClipServer.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ShareClipClient src/main/ShareClipClient.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ShareClipServer src/main/ShareClipServer.go
+
+echo 'build windows application'
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ShareClipClient.exe src/main/ShareClipClient.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ShareClipServer.exe src/main/ShareClipServer.go
